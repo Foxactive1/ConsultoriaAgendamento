b2a1ae910e553729289f1f36de7e29dcf9e30756
Dione Castro Alves <84285490+Foxactive1@users.noreply.github.com>
2025-11-02 09:48:44 -0300

    Update Agendamento.cs

--- models/Agendamento.cs
+++ models/Agendamento.cs
@@ -1,1 +1,17 @@
 
+using System;
+
+namespace ConsultoriaAgendamentos.Models
+{
+    public class Agendamento
+    {
+        public string NomeCliente { get; set; }
+        public DateTime Data { get; set; }
+        public string Descricao { get; set; }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"{Data:dd/MM/yyyy} - {NomeCliente}: {Descricao}");
+        }
+    }
+}

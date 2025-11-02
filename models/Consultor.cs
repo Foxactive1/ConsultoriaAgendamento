6c249955fd838cfb39e357545ca9ad244be72137
Dione Castro Alves <84285490+Foxactive1@users.noreply.github.com>
2025-11-02 09:49:28 -0300

    Update Consultor.cs

--- models/Consultor.cs
+++ models/Consultor.cs
@@ -1,1 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsultoriaAgendamentos.Models
+{
+    public class Consultor
+    {
+        public string Nome { get; set; }
+        public List<Agendamento> Agendamentos { get; set; } = new List<Agendamento>();
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"{Nome}:");
+
+            if (Agendamentos.Count == 0)
+            {
+                Console.WriteLine("Nenhum agendamento cadastrado");
+            }
+            else
+            {
+                foreach (var ag in Agendamentos.OrderBy(a => a.Data))
+                {
+                    ag.Exibir();
+                }
+            }
+        }
+    }
+}
 
